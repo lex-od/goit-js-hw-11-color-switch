83b57148152575f21209e1edc8886ab67b450c23
Aleksey Yesin <esinru@gmail.com>
2021-03-08 20:18:36 +0200

    Color switch v1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 import './styles.css';
-
-const colors = [
-  '#FFFFFF',
-  '#2196F3',
-  '#4CAF50',
-  '#FF9800',
-  '#009688',
-  '#795548',
-];
+import colors from './data/colors.json';
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+const toggleBtnsAccess = () => {
+  btnStartRef.disabled = !btnStartRef.disabled;
+  btnStopRef.disabled = !btnStopRef.disabled;
+};
+
+const startColorTimer = () =>
+  setInterval(() => {
+    const rndColor = randomIntegerFromInterval(0, colors.length - 1);
+    document.body.style.backgroundColor = colors[rndColor];
+  }, 1000);
+
+const stopColorTimer = id => clearInterval(id);
+
+let timerId = null;
+
+const btnStartRef = document.querySelector('[data-action="start"]');
+const btnStopRef = document.querySelector('[data-action="stop"]');
+
+btnStartRef.addEventListener('click', () => {
+  timerId = startColorTimer();
+  toggleBtnsAccess();
+});
+
+btnStopRef.addEventListener('click', () => {
+  stopColorTimer(timerId);
+  toggleBtnsAccess();
+});
